9698a4ecb7ecd4edd6849ccb9c6c47a01289d12c
Gareth Townsend <quamen@gmail.com>
2009-03-09 16:26:54 +1100

    Re-organising.

--- Final/Day 2/02 todo/app/models/list.rb
+++
@@ -1,6 +0,0 @@
-class List < ActiveRecord::Base
-  validates_presence_of :title
-  validates_uniqueness_of :title
-  
-  has_many :todos
-end
--- Final/Day 2/02 todo/app/models/todo.rb
+++
@@ -1,6 +0,0 @@
-class Todo < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :list
-  
-  belongs_to :list
-end
