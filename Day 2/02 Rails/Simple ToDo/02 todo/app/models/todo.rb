f18bbc01e6c984eab5536bb7e63e1e6bbdf1ccb8
Gareth Townsend <quamen@gmail.com>
2009-03-12 11:44:55 +1100

    Remove working files, they are now in in the working-copy directory.

--- Day 2/02 Rails/Simple ToDo/02 todo/app/models/todo.rb
+++
@@ -1,7 +0,0 @@
-class Todo < ActiveRecord::Base
-  validates_presence_of :title, :list_id
-
-  belongs_to :list, :class_name => "List", :foreign_key => "list_id"
-  
-  belongs_to :person, :class_name => "Person", :foreign_key => "person_id"
-end
--- Day 2/02 Rails/Simple ToDo/02 todo/db/migrate/20080928053618_add_person_to_todo_table.rb
+++
@@ -1,9 +0,0 @@
-class AddPersonToTodoTable < ActiveRecord::Migration
-  def self.up
-    add_column :todos, :person_id, :string
-  end
-
-  def self.down
-    remove_column :todos, :person_id
-  end
-end
