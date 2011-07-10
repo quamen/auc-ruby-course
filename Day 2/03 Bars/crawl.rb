8ae8cc61aae90e29083b47ad2a21519dab09ff1f
Marcus Crafter <crafterm@redartisan.com>
2011-07-10 11:50:47 +1000

    Whitespace

--- Day 2/03 Bars/crawl.rb
+++ Day 2/03 Bars/crawl.rb
@@ -1,12 +1,12 @@
 crawl do
-  
+
   pub :name => 'Mitre' do
     address do
       street 'Some lane'
       lng 123.21
       lat 45.34
     end
-    
+
     beers do
       coopers
       asahi
@@ -14,27 +14,26 @@ crawl do
       carlton
       cascade
     end
-    
+
     crowd :mood => :bankers
-    
+
   end
-  
+
   bar :name => '1806' do
-    
+
     address do
       street 'Exhibition Street'
       lng 123.21
       lat 45.34
     end
-    
+
     cocktails do
       appletini
-      sex_on_the_beach
       caipraroska
     end
-    
+
     crowd :mood => :legends
-    
+
   end
-  
+
 end
